128fbf6fec12bf51ff801645f63c8e0817cd2e48
Kalana-indula <kalanasmedia@outlook.com>
2025-11-05 16:13:06 +0530

    hero section stage 2 completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import Hero from '@/components/Hero';
 import React from 'react'
+import {FloatingNav} from "@/components/ui/floating-navbar";
+import {FaHome} from "react-icons/fa";
+import Grid from "@/components/Grid";
+import {House} from "lucide-react";
 
 const Home = () => {
-  return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <Hero/>
-      </div>
-    </main>
-  )
+    return (
+        <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+            <div className="max-w-7xl w-full">
+                <FloatingNav navItems={[
+                    {name: 'Home', link: '/', icon: <House strokeWidth={1} />}
+                ]}/>
+                <Hero/>
+                <Grid/>
+            </div>
+        </main>
+    )
 }
 
 export default Home;
