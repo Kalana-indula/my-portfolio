add64c8bfb797ce2816292b40eade1d62b6810d1
Kalana-indula <kalanasmedia@outlook.com>
2025-11-06 14:07:56 +0530

    project section added

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,17 @@ import {FloatingNav} from "@/components/ui/floating-navbar";
 import {FaHome} from "react-icons/fa";
 import Grid from "@/components/Grid";
 import {House} from "lucide-react";
+import RecentProjects from "@/components/RecentProjects";
+import {navItems} from "@/data";
 
 const Home = () => {
     return (
         <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
             <div className="max-w-7xl w-full">
-                <FloatingNav navItems={[
-                    {name: 'Home', link: '/', icon: <House strokeWidth={1} />}
-                ]}/>
+                <FloatingNav navItems={navItems}/>
                 <Hero/>
                 <Grid/>
+                <RecentProjects/>
             </div>
         </main>
     )
